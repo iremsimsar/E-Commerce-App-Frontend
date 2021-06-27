d14a9d48954be7796e613e385bb9c9c54368689d
esratiras <16110131019@ogr.ksu.edu.tr>
2021-06-27 19:07:56 +0300

    Anasayfa tema giydirilmesi

--- src/main.js
+++ src/main.js
@@ -3,6 +3,5 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
-import '/public/css/style.css'
 
 createApp(App).use(store).use(router).mount("#app");
--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,32 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Login from "../views/Login/Login.vue";
 import Register from "../views/Login/Register";
-const routes = [
+import Index from "../views/Index/Index";
+const routes = [{
+        path: "/index",
+        name: "Index",
 
-  {
-    path: "/login",
-    name: "Login",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: Login,
-  },
-  {
-    path: "/register",
-    name: "Register",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: Register,
-  },
+        component: Index,
+    },
+    {
+        path: "/login",
+        name: "Login",
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: Login,
+    },
+    {
+        path: "/register",
+        name: "Register",
+
+        component: Register,
+    },
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes,
+    history: createWebHistory(process.env.BASE_URL),
+    routes,
 });
 
 export default router;
